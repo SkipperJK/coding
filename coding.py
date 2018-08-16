73797f0a049511f925eac8dd56e76afb4cf59d62
SkipperJK <502456370@qq.com>
2018-08-16 10:39:21 +0800

    Convert gbxxx and not utf8 encoding to utf8, ISO-8895 cannot

--- coding.py
+++ coding.py
@@ -8,26 +8,35 @@ count = 0
 if sys.getdefaultencoding() != "utf-8":
 	reload(sys)
 	sys.setdefaultencoding("utf-8")
+gb = ['gbk', 'gb2312', 'gb18030']
+
+
+def usage():
+	print "---------------Instruction----------------------"
+	print "-----1  python coding.py xxx(file)--------------"
+	print "-----2  python coding.py xxx(dir) --------------"
+	print "-----3  python coding.py xxx(dir) R(recursive)--"
+	print "------------------------------------------------"
 
 
 def reading(path):
 	try:
-		f = open(path, "r")
+		f = open(path, "rb") 
+                #Using rb reading the binary code, aviod coding transfer eror
 		data = f.read()
-		# ***
-		# data.replace('\r\n', '\n')
-
-
 		f.close()
 		preEncoding = chardet.detect(data)['encoding'].lower()
 		# print preEncoding
-		if(preEncoding == 'gb2312' or preEncoding == 'gbk' or preEncoding == 'gb18030'):
+                if preEncoding in gb:
 			uData = data.decode('gb18030')
 			return 1, uData
-		else:
-			return 0, False
+                else:
+                        if preEncoding == 'utf-8': # if file encoding is utf-8, do nothing
+                        	return 0, 0
+                        else:
+                                uData = data.decode(preEncoding)
+				return 1, uData
 	except:
-		# print "Reading Error!"
 		return -1, False
 
 
@@ -41,80 +50,47 @@ def writing(path, data):
 		return -1
 
 
-def convertCoding(path, recursive=False):
+def main(path, recursive=False):
 	if os.path.isfile(path):
 		if os.path.splitext(path)[1] == '.txt':
 			# print "File---"+path
 			global count
 			count += 1
 			status_read, uData = reading(path)
-			if status_read == 0:
-				print "Already UTF-8 encoding!"
-				return 0
-
-			elif status_read == -1:
-				print "Reading error!"
-				print path
-				return 0
-
-			elif status_read == 1:
-			# if  uData:
+			if status_read == 1:
 				try:
 					data = uData.encode("utf-8")
-				# print data
 				except:
-					print "Data Encode error!"
-					print path
+					print "Data Encode error! \n\tFile: %s"%path
 					return 0
-
 				status_write = writing(path, data)
 				if status_write == 1:
 					print "Finish convertCoding!"
 
 				elif status_write == -1:
-					print "Writing error!"
-					print path
+					print "Writing error! \n\tFile: %s"%path
 					return 0
-					
+			elif status_read == -1:
+				print "Reading error! \n\tFile: %s"%path
+				return 0		
 	elif os.path.isdir(path) == True:
-		print "Directory--"+path
+		print "----Directory----: "+path
 		for i in os.listdir(path):
 			subPath = os.path.join(path, i)
-			# print subPath
-			# convertCoding(subPath)
-			if recursive:
-				convertCoding(subPath, recursive)
-			else:
-				convertCoding(subPath)
-
-		
-
-
-
-def usage():
-	print "---------------instruction----------------"
-	print "-----1--python xxx(file)------------------"
-	print "-----2--python xxx(dir) ------------------"
-	print "-----3--python xxx(dir) recursive---------"
-	print "------------------------------------------"
-
+			if os.path.isdir(subPath) and recursive:
+				main(subPath, recursive)
+			elif os.path.isfile(subPath):
+				main(subPath)
 
 
 if __name__ == '__main__':
-	if len(sys.argv) == 1:
-		usage()
-		exit()
-	if len(sys.argv) > 3:
+	if len(sys.argv) == 1 or len(sys.argv) > 3:
 		usage()
 		exit()
 	path = sys.argv[1]
 	if not os.path.exists(path):
-		usage()
 		print "This file or dir is not exist!"
 		exit()
-	# cnt = 0
-	recursive = (len(sys.argv) == 3 and sys.argv[2] == 'recursive')
-	convertCoding(path, recursive)
+	recursive = (len(sys.argv) == 3 and sys.argv[2] == 'R')
+	main(path, recursive)
 	print  count
-
-
